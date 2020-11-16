184fc055203c581a3974626d003e80d259f20ece
ThomasCWright <tcw2512@gmail.com>
2020-11-16 09:48:59 +0100

    Hello world app tutorial added

--- tests/step_defs/test_print_sudoku_steps.py
+++ tests/step_defs/test_print_sudoku_steps.py
@@ -2,9 +2,3 @@ from pytest_bdd import scenario, given, when, then
 from pytest_bdd.parsers import cfparse
 
 
-
-
-@given(cfparse('the {basket} has {2} cucumbers'))
-def step_function(, basket, 2):
-    # Add Your Code Here
-    pass
