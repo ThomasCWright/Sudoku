31127c81cb3463ca58e133ef2deb717798e809c9
ThomasCWright <tcw2512@gmail.com>
2020-11-21 21:15:04 +0100

    Hello world working on phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.11.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
@@ -8,7 +8,7 @@ from kivy.uix.label import Label
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return Label(text=f"Hello world!")
 
 
 if __name__ == '__main__':
